78591f3fe8c97f6f2f837071f6b5715a9cb1f2dc
unknown <nahidhasan9613@gmail.com>
2025-05-18 22:37:57 +0600

    create chat_summarizer.py file

---
+++ chat_summarizer.py
@@ -0,0 +1,20 @@
+import string
+from collections import Counter
+
+def main():
+    file_path = 'chat.txt'
+    stop_words = {
+        'the', 'is', 'and', 'a', 'an', 'in', 'it', 'to', 'of', 'for', 'you', 'your',
+        'i', 'me', 'my', 'we', 'our', 'he', 'she', 'they', 'them', 'this', 'that',
+        'these', 'those', 'be', 'are', 'was', 'were', 'have', 'has', 'had', 'do',
+        'does', 'did', 'can', 'could', 'will', 'would', 'should', 'what', 'where',
+        'when', 'how', 'why', 'if', 'then', 'there', 'here', 'so', 'not', 'with',
+        'as', 'at', 'on', 'up', 'down', 'out', 'about', 'into', 'over', 'under',
+        'again', 'further', 'once', 'more', 'most', 'very', 'just', 'now', 'than',
+        'no', 'yes', 'am', 'pm'
+    }
+
+    
+
+if __name__ == "__main__":
+    main()
